bc7b2ad0fa8a10b463d3462933dfb137786522e6
Dmitriy <d.razumovskiy@kupibilet.ru>
2019-12-24 23:20:32 +0300

    work

--- tests/poll.rs
+++ tests/poll.rs
@@ -1,10 +1,10 @@
-extern crate libc;
-use std::{io::{self, Write}, os::unix::{io::AsRawFd, net}};
+use std::{io::{self, Write, Read}, os::unix::{io::AsRawFd, net}};
+const MESSAGE: &'static [u8] = b"Hello World";
 
 #[test]
 fn test_poll_add() -> io::Result<()> {
     let mut ring = iou::IoUring::new(2)?;
-    let (read, mut write) = net::UnixStream::pair()?;
+    let (mut read, mut write) = net::UnixStream::pair()?;
     unsafe {
         let mut sqe = ring.next_sqe().expect("failed to get sqe");
         sqe.prep_poll_add(read.as_raw_fd(), iou::PollFlags::POLLIN);
@@ -12,13 +12,15 @@ fn test_poll_add() -> io::Result<()> {
         ring.submit_sqes()?;
     }
 
-    write.write(&[1])?;
+    write.write(MESSAGE)?;
 
     let cqe = ring.wait_for_cqe()?;
     assert_eq!(cqe.user_data(), 0xDEADBEEF);
     let mask = unsafe { iou::PollFlags::from_bits_unchecked(cqe.result()? as _) };
     assert!(mask.contains(iou::PollFlags::POLLIN));
-
+    let mut buf = [0; MESSAGE.len()];
+    read.read(&mut buf);
+    assert_eq!(buf, MESSAGE);
     Ok(())
 }
 
